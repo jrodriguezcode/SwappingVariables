ea652150b26bf5c322d69d9086fc86e75a7e534f
jrodriguezcode <jr698751@gmail.com>
2025-02-03 13:35:53 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String x = "water";
+		String y = "Kool-Aid";
+		
+		// x = y; Both became Kool-Aid
+		// y = x; Both became water
+		
+		String temp;
+		
+		System.out.println("x: " + x);
+		System.out.println("y: " + y);
+		
+		temp = x;
+		x = y;
+		y = temp;
+		
+		System.out.println("x changed to: " + x);
+		System.out.println("y changed to: " + y);
+
+	}
+
+}
